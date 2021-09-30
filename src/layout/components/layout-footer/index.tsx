42c0e33d145a0652c8158cbf27077e58acb1bda7
Vladislav Ivanov <ivanov.vladdy@gmail.com>
2021-09-30 15:57:11 +0300

    Removed the telegram link

--- src/layout/components/layout-footer/index.tsx
+++ src/layout/components/layout-footer/index.tsx
@@ -66,10 +66,6 @@ const LayoutFooter: React.FC = () => {
               <ExternalLink href="https://www.coingecko.com/en/coins/enterdao" className={s.sLink}>
                 <Icon name="coingecko" width="20" height="20" />
               </ExternalLink>
-
-              <ExternalLink href="https://t.me/joinchat/ftcnxgCO36owNGNk" className={s.sLink}>
-                <Icon name="png/telegram" width="20" height="20" />
-              </ExternalLink>
               <ExternalLink href="https://medium.com/enterdao" className={s.sLink}>
                 <Icon name="medium" width="20" height="20" />
               </ExternalLink>
