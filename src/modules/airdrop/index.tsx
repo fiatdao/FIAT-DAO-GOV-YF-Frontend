1e9df36315c363c52378de3ee5a31485d8aaecf9
deluxart <deluxart3@gmail.com>
2021-11-22 03:15:43 +0200

    Add animations / add responsive

--- src/modules/airdrop/index.tsx
+++ src/modules/airdrop/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import cn from 'classnames';
+import Lottie from 'lottie-react';
 
 import Table from 'components/antd/table';
 import Grid from 'components/custom/grid';
@@ -8,11 +9,13 @@ import ExternalLink from '../../components/custom/externalLink';
 import Icon from '../../components/custom/icon';
 import { Hint, Text } from '../../components/custom/typography';
 import useMediaQuery from '../../hooks/useMediaQuery';
+import waveAnimations from './animation/waves.json';
 
 import { getEtherscanTxUrl, shortenAddr } from '../../web3/utils';
 
 import s from './AirDropPage.module.scss';
 
+const progressPercent = 50;
 const columns = [
   {
     dataIndex: 'address',
@@ -116,10 +119,101 @@ const data = [
       </div>
     ),
   },
+  {
+    key: '5',
+    address: (
+      <ExternalLink href={getEtherscanTxUrl('0x22A674F22Dafc25a16A29aD5c2a7710ceE1076FF')} className="link-blue mb-4">
+        <Text type="p1" weight="semibold" color="var(--gradient-blue-safe)" textGradient="var(--gradient-blue)">
+          {shortenAddr('0x22A674F22Dafc25a16A29aD5c2a7710ceE1076FF')}
+        </Text>
+      </ExternalLink>
+    ),
+    amount: (
+      <div className="flex flow-col align-center justify-end">
+        <Icon width={19} height={19} name="png/fiat-dao" className="mr-4" />
+        <Text type="p2" weight="700" color="primary">
+          100,000
+        </Text>
+      </div>
+    ),
+  },
+  {
+    key: '6',
+    address: (
+      <ExternalLink href={getEtherscanTxUrl('0x22A674F22Dafc25a16A29aD5c2a7710ceE1076FF')} className="link-blue mb-4">
+        <Text type="p1" weight="semibold" color="var(--gradient-blue-safe)" textGradient="var(--gradient-blue)">
+          {shortenAddr('0x22A674F22Dafc25a16A29aD5c2a7710ceE1076FF')}
+        </Text>
+      </ExternalLink>
+    ),
+    amount: (
+      <div className="flex flow-col align-center justify-end">
+        <Icon width={19} height={19} name="png/fiat-dao" className="mr-4" />
+        <Text type="p2" weight="700" color="primary">
+          100,000
+        </Text>
+      </div>
+    ),
+  },
+  {
+    key: '7',
+    address: (
+      <ExternalLink href={getEtherscanTxUrl('0x22A674F22Dafc25a16A29aD5c2a7710ceE1076FF')} className="link-blue mb-4">
+        <Text type="p1" weight="semibold" color="var(--gradient-blue-safe)" textGradient="var(--gradient-blue)">
+          {shortenAddr('0x22A674F22Dafc25a16A29aD5c2a7710ceE1076FF')}
+        </Text>
+      </ExternalLink>
+    ),
+    amount: (
+      <div className="flex flow-col align-center justify-end">
+        <Icon width={19} height={19} name="png/fiat-dao" className="mr-4" />
+        <Text type="p2" weight="700" color="primary">
+          100,000
+        </Text>
+      </div>
+    ),
+  },
+  {
+    key: '8',
+    address: (
+      <ExternalLink href={getEtherscanTxUrl('0x22A674F22Dafc25a16A29aD5c2a7710ceE1076FF')} className="link-blue mb-4">
+        <Text type="p1" weight="semibold" color="var(--gradient-blue-safe)" textGradient="var(--gradient-blue)">
+          {shortenAddr('0x22A674F22Dafc25a16A29aD5c2a7710ceE1076FF')}
+        </Text>
+      </ExternalLink>
+    ),
+    amount: (
+      <div className="flex flow-col align-center justify-end">
+        <Icon width={19} height={19} name="png/fiat-dao" className="mr-4" />
+        <Text type="p2" weight="700" color="primary">
+          100,000
+        </Text>
+      </div>
+    ),
+  },
+  {
+    key: '9',
+    address: (
+      <ExternalLink href={getEtherscanTxUrl('0x22A674F22Dafc25a16A29aD5c2a7710ceE1076FF')} className="link-blue mb-4">
+        <Text type="p1" weight="semibold" color="var(--gradient-blue-safe)" textGradient="var(--gradient-blue)">
+          {shortenAddr('0x22A674F22Dafc25a16A29aD5c2a7710ceE1076FF')}
+        </Text>
+      </ExternalLink>
+    ),
+    amount: (
+      <div className="flex flow-col align-center justify-end">
+        <Icon width={19} height={19} name="png/fiat-dao" className="mr-4" />
+        <Text type="p2" weight="700" color="primary">
+          100,000
+        </Text>
+      </div>
+    ),
+  },
 ];
 
 const AirDropPage = () => {
-  const isMobile = useMediaQuery(992);
+  const isTablet = useMediaQuery(992);
+  const isMobile = useMediaQuery(720);
   return (
     <section className={s.page}>
       <div className={s.container}>
@@ -132,8 +226,8 @@ const AirDropPage = () => {
           <br /> passed will forfeit a portion of your balance. Your total claimable amount will rise whenever someone
           forfeits a portion of their reward.
         </Text>
-        <Grid flow="col" colsTemplate="1fr 350px" gap={30} className="mb-12">
-          <div>
+        <Grid colsTemplate={!isTablet ? '1fr 350px' : '1fr'} gap={30} className="mb-12">
+          <Grid rowsTemplate="auto 1fr auto">
             <Grid
               colsTemplate={!isMobile ? '1fr 1fr 1fr' : '1fr'}
               gap={24}
@@ -184,55 +278,105 @@ const AirDropPage = () => {
                 <Text type="small">WEEK 15/100</Text>
               </div>
               <div className={s.bigBlock}>
-                <Hint text="Your total airdrop amount - tooltip" className="mb-12">
-                  <Text type="small" color="secondary">
-                    Your total airdrop amount
-                  </Text>
-                </Hint>
-                <div className="flex flow-col align-center mb-60">
-                  <Icon width={40} height={40} name="png/fiat-dao" className="mr-4" />
-                  <Text type="h1" weight="700" color="primary">
-                    135,000
-                  </Text>
+                <div>
+                  <Hint text="Your total airdrop amount - tooltip" className="mb-12">
+                    <Text type="small" color="secondary">
+                      Your total airdrop amount
+                    </Text>
+                  </Hint>
+                  <div className="flex flow-col align-center mb-48">
+                    <Icon width={40} height={40} name="png/fiat-dao" className="mr-4" />
+                    <Text type="h1" weight="700" color="primary">
+                      135,000
+                    </Text>
+                  </div>
+                  <Hint text="Your airdrop amount - tooltip" className="mb-12">
+                    <Text type="small" color="secondary">
+                      Your airdrop amount
+                    </Text>
+                  </Hint>
+                  <div className="flex flow-col align-center mb-32">
+                    <Icon width={19} height={19} name="png/fiat-dao" className="mr-4" />
+                    <Text type="p2" weight="700" color="primary">
+                      500,000
+                    </Text>
+                  </div>
+                  <Hint text="Your airdrop amount - tooltip" className="mb-12">
+                    <Text type="small" color="secondary">
+                      Your bonus amount
+                    </Text>
+                  </Hint>
+                  <div className="flex flow-col align-center">
+                    <Icon width={19} height={19} name="png/fiat-dao" className="mr-4" />
+                    <Text type="p2" weight="700" color="green">
+                      500,000
+                    </Text>
+                  </div>
                 </div>
-                <Hint text="Your airdrop amount - tooltip" className="mb-12">
-                  <Text type="small" color="secondary">
-                    Your airdrop amount
-                  </Text>
-                </Hint>
-                <div className="flex flow-col align-center mb-32">
-                  <Icon width={19} height={19} name="png/fiat-dao" className="mr-4" />
-                  <Text type="p2" weight="700" color="primary">
-                    500,000
-                  </Text>
-                </div>
-                <Hint text="Your airdrop amount - tooltip" className="mb-12">
-                  <Text type="small" color="secondary">
-                    Your bonus amount
-                  </Text>
-                </Hint>
-                <div className="flex flow-col align-center">
-                  <Icon width={19} height={19} name="png/fiat-dao" className="mr-4" />
-                  <Text type="p2" weight="700" color="green">
-                    500,000
-                  </Text>
+                <div className={s.progress}>
+                  <div>
+                    <span>
+                      <Text type="h1" weight="700" color="primary">
+                        100,000
+                      </Text>
+                      <Text type="p3" color="primary">
+                        available
+                      </Text>
+                    </span>
+                    <Lottie
+                      animationData={waveAnimations}
+                      style={{ transform: `translateY(calc(-${progressPercent}% - -10px))` }}
+                      className={s.waveAnimation}
+                    />
+                  </div>
                 </div>
               </div>
             </div>
             <div>
               <div className={s.cardGradient}>
-                <div>Test block</div>
+                <div>
+                  <Grid colsTemplate={!isTablet ? '1fr 1fr auto' : '1fr'} align="center" gap={24}>
+                    <div>
+                      <Text type="small" color="secondary" className="mb-4">
+                        Available to claim now:
+                      </Text>
+                      <div className="flex flow-col align-center">
+                        <Icon width={19} height={19} name="png/fiat-dao" className="mr-4" />
+                        <Text type="h2" weight="700" color="primary">
+                          100,000
+                        </Text>
+                      </div>
+                    </div>
+                    <div>
+                      <Text type="small" color="secondary" className="mb-4">
+                        Available to claim now:
+                      </Text>
+                      <div className="flex flow-col align-center">
+                        <Icon width={19} height={19} name="png/fiat-dao" className="mr-4" />
+                        <Text type="h2" weight="700" color="primary" textGradient="var(--gradient-pink)">
+                          100,000
+                        </Text>
+                      </div>
+                    </div>
+                    <button className="button-primary">Claim</button>
+                  </Grid>
+                </div>
               </div>
             </div>
-          </div>
-          <div className={s.card}>
+          </Grid>
+          <div className={cn(s.card, s.card__table)}>
             <Hint text="Total redistributed - tooltip" className="mt-16 mb-32">
               <Text type="small" color="secondary">
                 Last claimed
               </Text>
             </Hint>
             <div className={s.table}>
-              <Table showHeader={false} columns={columns} dataSource={data} />
+              <Table
+                showHeader={false}
+                pagination={{ pageSize: 8, position: ['bottomCenter'], defaultPageSize: 8, pageSizeOptions: ['8'] }}
+                columns={columns}
+                dataSource={data}
+              />
             </div>
           </div>
         </Grid>
