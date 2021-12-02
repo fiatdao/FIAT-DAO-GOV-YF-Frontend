a7a95771fbeffc8334555d9ab9a9907e317b3ee4
Konstantin Stepakh <1ordcommander@duck.com>
2021-12-02 19:21:17 +0200

    Update userAmount

--- src/modules/rewards/views/airdrop/index.tsx
+++ src/modules/rewards/views/airdrop/index.tsx
@@ -33,7 +33,7 @@ const AirDropPage = () => {
   const totalClaimed = new _BigNumber(merkleDistributorContract?.totalInfo?.totalFDTAirdropClaimed ?? 0).unscaleBy(FDTToken.decimals)
   const totalRedistributed = new _BigNumber(merkleDistributorContract?.totalInfo?.totalFDTAirdropRedistributed ?? 0).unscaleBy(FDTToken.decimals)
 
-  const userAmount = new _BigNumber(merkleDistributorContract?.claimAmount ?? 0)
+  const userAmount = new _BigNumber(merkleDistributorContract?.claimAmount ?? 0).unscaleBy(FDTToken.decimals)
   const userAvailable = new _BigNumber(merkleDistributorContract?.adjustedAmount?.airdropAmount ?? 0).unscaleBy(FDTToken.decimals)
   const userBonus = new _BigNumber(merkleDistributorContract?.adjustedAmount?.bonus ?? 0).unscaleBy(FDTToken.decimals)
 
